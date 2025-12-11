46d8dd047e9fa69c6fdee0ed3b17567da2e1c420
Neftalí Alcalá <21018873@es.uveg.edu.mx>
2025-12-11 10:18:08 -0600

    Corrección módulo usuarios: ya no se puede entrar por rutas, se usan gates para restringir modificaciones de usuarios, se redirigen las rutas en caso de no estar autentificado

--- routes/web.php
+++ routes/web.php
@@ -167,9 +167,20 @@ Route::prefix('loginTickets')->group(function(){
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
-//Registro de Usuarios
-Route::get('/usuarios/inicio', 'ControladorPanelUsuarios@mostrarInicioUsuarios')->name('usuarios.inicio');
-Route::post('/usuarios/inicio/registro', 'ControladorPanelUsuarios@registrarUsuario')->name('usuarios.registro');
-Route::post('/usuarios/inicio/modificacion/{nombreClaveUsuario}', 'ControladorPanelUsuarios@modificarUsuario')->name('usuarios.modificacion');
-Route::post('/usuarios/inicio/eliminacion/{nombreClaveUsuario}', 'ControladorPanelUsuarios@eliminarUsuario')->name('usuarios.eliminacion');
-Route::get('/usuarios/inicio/registro/modificacion/usuarios', 'ControladorPanelUsuarios@mostrarRegistroModificaciones')->name('usuarios.registro.modificacion');
+/*Route::middleware('auth')->group(function () {
+    //Registro de Usuarios
+    Route::get('/usuarios/inicio', 'ControladorPanelUsuarios@mostrarInicioUsuarios')->name('usuarios.inicio');
+    Route::post('/usuarios/inicio/registro', 'ControladorPanelUsuarios@registrarUsuario')->name('usuarios.registro');
+    Route::post('/usuarios/inicio/modificacion/{nombreClaveUsuario}', 'ControladorPanelUsuarios@modificarUsuario')->name('usuarios.modificacion');
+    Route::post('/usuarios/inicio/eliminacion/{nombreClaveUsuario}', 'ControladorPanelUsuarios@eliminarUsuario')->name('usuarios.eliminacion');
+    Route::get('/usuarios/inicio/registro/modificacion/usuarios', 'ControladorPanelUsuarios@mostrarRegistroModificaciones')->name('usuarios.registro.modificacion');
+});*/
+
+Route::middleware(['auth', 'can:manage-users'])->group(function () {
+    Route::get('/usuarios/inicio', 'ControladorPanelUsuarios@mostrarInicioUsuarios')->name('usuarios.inicio');
+    Route::post('/usuarios/inicio/registro', 'ControladorPanelUsuarios@registrarUsuario')->name('usuarios.registro');
+    Route::post('/usuarios/inicio/modificacion/{nombreClaveUsuario}', 'ControladorPanelUsuarios@modificarUsuario')->where('nombreClaveUsuario', '[A-Za-z0-9_-]+')->name('usuarios.modificacion');
+    Route::post('/usuarios/inicio/eliminacion/{nombreClaveUsuario}', 'ControladorPanelUsuarios@eliminarUsuario')->where('nombreClaveUsuario', '[A-Za-z0-9_-]+')->name('usuarios.eliminacion');
+    Route::get('/usuarios/inicio/registro/modificacion/usuarios', 'ControladorPanelUsuarios@mostrarRegistroModificaciones')->name('usuarios.registro.modificacion');
+});
+
